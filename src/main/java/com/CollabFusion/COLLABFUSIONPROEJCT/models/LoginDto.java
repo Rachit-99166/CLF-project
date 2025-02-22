6f377d34d18ab55216a59ed42901c7c827a1a941
Rachit <rachitdua997@gmail.com>
2025-02-22 12:41:03 +0530

    Update LoginDto.java

--- src/main/java/com/CollabFusion/COLLABFUSIONPROEJCT/models/LoginDto.java
+++ src/main/java/com/CollabFusion/COLLABFUSIONPROEJCT/models/LoginDto.java
@@ -7,7 +7,7 @@ public class LoginDto {
     @NotEmpty
     private String username;
     @NotEmpty
-    private String password;
+    private String password; 
 
     public String getUsername() {
         return username;
