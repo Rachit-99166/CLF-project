2f13f607cfe3258cf9dfdfae950a9fcd3474a751
Rachit <rachitdua997@gmail.com>
2025-02-24 13:40:12 +0530

    Update ClientsController.java

--- src/main/java/com/CollabFusion/COLLABFUSIONPROEJCT/controllers/ClientsController.java
+++ src/main/java/com/CollabFusion/COLLABFUSIONPROEJCT/controllers/ClientsController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.CollabFusion.COLLABFUSIONPROEJCT.models.Client;
 import com.CollabFusion.COLLABFUSIONPROEJCT.repositories.ClientsRepository;
 
-@Controller
+@Controller 
 @RequestMapping("/clients")
 public class ClientsController { 
  
